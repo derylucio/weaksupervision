24304b8f08dd43b3dae33ed601614b451e76a46f
francescorubbo <rubbo.francesco@gmail.com>
2016-12-16 18:44:33 -0800

    use customizable layersize for full supervision

--- models.py
+++ models.py
@@ -21,12 +21,12 @@ def traincomplete(trainsamples,trainlabels,nb_epoch):
                                  validation_split=0.2)
     return model_complete
 
-def trainqgcomplete(trainsamples,trainlabels,nb_epoch):
+def trainqgcomplete(trainsamples,trainlabels,nb_epoch,layersize=10):
     X_train = np.concatenate( trainsamples )
     y_train = np.concatenate( trainlabels )
     
     model_complete = Sequential()
-    model_complete.add( Dense(10, input_dim=(X_train.shape[1]), 
+    model_complete.add( Dense(layersize, input_dim=(X_train.shape[1]), 
                               init='normal', activation='sigmoid') )
     model_complete.add( Dense(1, init='normal', activation='sigmoid') )
     model_complete.compile(loss='mean_squared_error', optimizer='sgd')
@@ -50,7 +50,7 @@ def getweak(inputsize,layersize,l2reg=0,sdreg=0):
         loss2 = (1.0 - K.sum(ypred)/ypred.shape[0]) - K.sum(ytrue)/ypred.shape[0]
         loss2 = K.square(loss2) - constrib
         loss = K.switch(T.lt(loss1, loss2), loss1, loss2)
-        return loss
+        return loss1
     
     model_weak = Sequential()
     model_weak.add(Dense(layersize, input_dim=(inputsize), 
